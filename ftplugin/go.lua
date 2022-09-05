bfc97139bbe0ddb8776cd646ac530063fd3ff40b
wsl <shoulinwang@gmail.com>
2022-09-05 21:38:34 +0800

    fix: set comment format

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,5 +1,9 @@
 local go = require('nvim-go')
 
+vim.bo.comments=[[s1:/*,mb:*,ex:*/,://]]
+vim.bo.commentstring=[[// %s]]
+vim.bo.expandtab = false
+
 vim.api.nvim_buf_create_user_command(0, "GoAddTag", function(a) go.tag(a.line1, a.line2, "add", a.args) end, {nargs='?', range=true})
 vim.api.nvim_buf_create_user_command(0, "GoRemoveTag", function(a) go.tag(a.line1, a.line2, "remove", a.args) end, {nargs='?', range=true})
 vim.api.nvim_buf_create_user_command(0, "GoModTidy", go.modtidy, {nargs=0})
