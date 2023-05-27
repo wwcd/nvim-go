e8fce20713a80e62ee88826e61391320d52fd114
wsl <shoulinwang@gmail.com>
2023-05-27 14:01:06 +0800

    feat: autocmd for gotmpl

--- ftdetect/gotmpl.lua
+++ ftdetect/gotmpl.lua
@@ -3,3 +3,19 @@ vim.filetype.add({
     gotmpl = "gotmpl",
   },
 })
+
+vim.api.nvim_create_autocmd(
+  {
+    "BufNewFile",
+    "BufRead",
+  },
+  {
+    pattern = {'*.yaml','*.yml','*.json','*.tpl'},
+    callback = function()
+      if vim.fn.search("{{.\\+}}", "nw") ~= 0 then
+        local buf = vim.api.nvim_get_current_buf()
+        vim.api.nvim_buf_set_option(buf, "filetype", "gotmpl")
+      end
+    end
+  }
+)
