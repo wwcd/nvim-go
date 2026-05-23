4239d633887b25defd89372adfedc8980e3a4fd2
wsl <wsl@noreply.pseudo>
2026-05-23 16:27:53 +0800

    fix: missing position_encoding

--- lua/nvim-go/utils.lua
+++ lua/nvim-go/utils.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.codeaction = function(action, only, wait_ms)
-  local params = vim.lsp.util.make_range_params()
+  local params = vim.lsp.util.make_range_params(0, "utf-8")
   params.context = { only = { only } }
   local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
   for _, res in pairs(result or {}) do
